53b8da9b893d171adb8e95fd8c0b0368a0d8ebdc
Nicolás Salas V <nicolas.salas@imfd.cl>
2020-01-10 15:23:20 -0300

    Define the store for the test

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,37 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+const state = {
+  loggedIn: false
+};
+
+const mutations = {
+  logUserIn(state) {
+    state.loggedIn = true;
+  },
+  destroySession(state) {
+    state.loggedIn = false;
+  }
+};
+
+const actions = {
+  // credentials should be { username, password }
+  authenticateUser: async ({ commit }, credentials) => {
+    let response = await axios.get("insert login url here", credentials);
+
+    if (response.status === 200) {
+      commit("logUserIn");
+    }
+  }
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state,
+  mutations,
+  actions
 });
+
+export { state, mutations, actions };
